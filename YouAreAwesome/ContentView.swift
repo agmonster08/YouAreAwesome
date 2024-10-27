b931f2733b2830e6bb44148d303dfa8c8d8a6c99
Arismendy Gill <arismendygill@gmail.com>
2024-10-26 22:26:47 -0400

    Spacers, SafeArea,CorenerRadius,Custom Colors & ZStack

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,54 +12,57 @@ struct ContentView: View {
     @State private var messageString: String = "When the Genius Bar needs help, they call you!"
     
     var body: some View {
-        //        VStack {
-        //            Image(systemName: "swift")
-        //                .resizable()
-        //                .scaledToFit()
-        //                .foregroundStyle(.orange)
-        //                .padding()
-        //            //                .padding(.bottom, 100)
-        //            //                .background(.blue)
-        //
-        //
-        //            Text("You Are Awesome!")
-        //                .font(.largeTitle)
-        //                .fontWeight(.semibold)
-        //                .foregroundStyle(.red)
-        //            //                .italic()
-        //            //                .background(.yellow)
-        //            //                .padding()
-        //
-        //        }
         
-        
-        VStack {
-            Text(messageString)
-                .font(.largeTitle)
-                .fontWeight(.semibold)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                            .frame(height: 150)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/) // Will expand the View's size to fit the size of its container
-                .border(.orange, width: 1)
-                .padding()
-            
-            HStack {
-                Button("Awesome") {
-                    // This is the action performed when the buttos is pressed
-                    messageString = "You Are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
+        ZStack {
+            Rectangle()
+                .fill(
+                    Gradient(colors: [])
+                )
+                .ignoresSafeArea()
+            VStack {
+                Text("You Have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundStyle(Color("Gold-BC"))
+                    .padding()
+                    .background(Color("Marron-BC"))
+                    .cornerRadius(15)
                 
-                Button("Great") {
-                    // This is the action performed when the buttos is pressed
-                    messageString = "You Are Great!"
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.red)
+                    .frame(height: 150)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/) // Will expand the View's size to fit the size of its container
+                //                .border(.orange, width: 1)
+                    .padding()
+                
+                Spacer()
+                
+                
+                HStack {
+                    Button("Awesome") {
+                        // This is the action performed when the buttos is pressed
+                        messageString = "You Are Awesome!"
+                    }
+                    .buttonStyle(.borderedProminent)
+                    
+                    Spacer()
+                    
+                    Button("Great") {
+                        // This is the action performed when the buttos is pressed
+                        messageString = "You Are Great!"
+                    }
+                    .buttonStyle(.borderedProminent)
                 }
-                .buttonStyle(.borderedProminent)
+                //            .border(.purple, width: 5)
+                .padding()
+                
             }
-            .border(.purple, width: 5)
-            
         }
         
     }
