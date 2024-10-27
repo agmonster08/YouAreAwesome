96e80e9701a020cae8492273ea6a7ef9b6b8790a
agmonster08 <26757790+agmonster08@users.noreply.github.com>
2024-10-26 23:19:11 -0400

    Group

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -14,58 +14,64 @@ struct ContentView: View {
     var body: some View {
         
         
-        GeometryReader { geometry in
-            VStack {
+        
+        VStack {
+            Group {
                 
-                Spacer()
-                
-                Text(messageString)
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/) // Will expand the View's size to fit the size of its container
-                //                .border(.orange, width: 1)
-                    .padding()
-                
-                Spacer()
-                
-                Divider()
-                    .background(.black)
-                    .padding()
-                    .frame(width: 150.0)
-                
-                
-                Rectangle()
-                    .fill(.indigo)
-                    .frame(width: geometry.size.width * (2/3), height: 1)
-                
-                
-                HStack {
-                    Button("Awesome") {
-                        // This is the action performed when the buttos is pressed
-                        messageString = "You Are Awesome!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                    
-                    Spacer()
-                    
-                    Button("Great") {
-                        // This is the action performed when the buttos is pressed
-                        messageString = "You Are Great!"
-                    }
-                    .buttonStyle(.borderedProminent)
-                }
-                //            .border(.purple, width: 5)
-                .padding()
-                
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a grup")
+                Text("This is part of a group")
             }
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.red)
+                .frame(height: 150)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/) // Will expand the View's size to fit the size of its container
+            //                .border(.orange, width: 1)
+                .padding()
+            
+            Spacer()
+            
+            
+            
+            HStack {
+                Button("Awesome") {
+                    // This is the action performed when the buttos is pressed
+                    messageString = "You Are Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                
+                Spacer()
+                
+                Button("Great") {
+                    // This is the action performed when the buttos is pressed
+                    messageString = "You Are Great!"
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            //            .border(.purple, width: 5)
+            .padding()
+            
         }
     }
-    
 }
+
 
 
 #Preview {
